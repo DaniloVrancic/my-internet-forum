3beaa07a21a2dc974aa9c56b8b6637d3590294a8
DaniloVrancic <danilov98@hotmail.com>
2024-05-02 23:50:55 +0200

    SNI 1.1: Added Spring Initalizr files with the proper added dependencies.

---
+++ my-forum-back/SecureForum/src/main/java/org/etf/unibl/SecureForum/SecureForumApplication.java
@@ -0,0 +1,13 @@
+package org.etf.unibl.SecureForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureForumApplication.class, args);
+	}
+
+}
---
+++ my-forum-back/SecureForum/src/test/java/org/etf/unibl/SecureForum/SecureForumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.etf.unibl.SecureForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
