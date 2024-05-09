317a5bc5faaab5441a77903d28c8aa6ef3f10519
DaniloVrancic <danilov98@hotmail.com>
2024-05-09 21:40:22 +0200

    SNI 1.6.4: Basic HTML for Register Page finished.

--- my-forum-front/my-forum-front-project/src/app/register-page/register-page/register-page.component.ts
+++ my-forum-front/my-forum-front-project/src/app/register-page/register-page/register-page.component.ts
@@ -1,19 +1,69 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RegisterRequest } from './registerRequest';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [ HttpClientModule ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css',
   providers: [Router]
 })
-export class RegisterPageComponent {
-constructor(private router: Router){}
+export class RegisterPageComponent implements OnInit{
 
-onSubmit() {
-throw new Error('Method not implemented.');
+  private myForm : any;
+  public isMyFormValid : boolean = false;
+  constructor(private router: Router,
+            private http: HttpClient
+            ){
+              
+            }
+
+  ngOnInit(): void {
+      this.myForm = document.forms[0];
+  }
+
+onInput(){
+  if(this.myForm.checkValidity())
+    {
+      this.isMyFormValid = true;
+    }
+  else
+  {
+    this.isMyFormValid = false;
+  }
+}
+
+
+registerUser() {
+  
+  
+  const formData = new FormData();
+  
+  let registerRequest : RegisterRequest = {} as RegisterRequest;
+
+  console.log(this.myForm);
+
+  /*
+  formData.append('username', this.username);
+  formData.append('password', this.password);
+  formData.append('email', this.email);
+  
+
+  this.http.post('your-api-endpoint', formData)
+    .subscribe(
+    {
+    next: response => {
+      // Handle success response
+    }, 
+    error: error => {},
+      // Handle error response
+    complete: () => {}
+    });
+    */
 }
 
 }
