08c0577120bfe361a4b333dc6a9674bd16722797
DaniloVrancic <danilov98@hotmail.com>
2024-05-09 21:12:29 +0200

    SNI 1.6.3: Started working on Register page. Basin HTML finished.

--- my-forum-front/my-forum-front-project/src/app/register-page/register-page/register-page.component.ts
+++ my-forum-front/my-forum-front-project/src/app/register-page/register-page/register-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
   imports: [],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
+  providers: [Router]
 })
 export class RegisterPageComponent {
+constructor(private router: Router){}
+
+onSubmit() {
+throw new Error('Method not implemented.');
+}
 
 }
