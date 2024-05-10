9f29a94bb922f5403e926f88358f72f1bd4c4a3e
DaniloVrancic <danilov98@hotmail.com>
2024-05-10 15:04:09 +0200

    SNI 1.6.8: Added a navbar for pages to navigate between various options available. (Admin page, Moderator page, and regular user (Forumer) page.

--- my-forum-front/my-forum-front-project/src/app/main-page/main-page/main-page.component.ts
+++ my-forum-front/my-forum-front-project/src/app/main-page/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationBarComponent } from '../../partials/nav/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [NavigationBarComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
