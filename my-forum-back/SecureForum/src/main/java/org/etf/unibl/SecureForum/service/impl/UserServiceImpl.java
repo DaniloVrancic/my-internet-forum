61869f729bf376901e364f7fb9728d190a1389e4
DaniloVrancic <danilov98@hotmail.com>
2024-05-11 15:35:48 +0200

    SNI 1.7.1: Finished the sign up method that sends emails and fills 2 database tables.

--- my-forum-back/SecureForum/src/main/java/org/etf/unibl/SecureForum/service/impl/UserServiceImpl.java
+++ my-forum-back/SecureForum/src/main/java/org/etf/unibl/SecureForum/service/impl/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package org.etf.unibl.SecureForum.service.impl;
 
 import jakarta.transaction.Transactional;
+import org.etf.unibl.SecureForum.additional.email.EmailSender;
 import org.etf.unibl.SecureForum.base.CrudJpaService;
 import org.etf.unibl.SecureForum.model.dto.User;
+import org.etf.unibl.SecureForum.model.entities.CodeVerificationEntity;
 import org.etf.unibl.SecureForum.model.entities.UserEntity;
 import org.etf.unibl.SecureForum.model.requests.ChangeRoleRequest;
 import org.etf.unibl.SecureForum.model.requests.ChangeStatusRequest;
 import org.etf.unibl.SecureForum.model.requests.SignUpRequest;
 import org.etf.unibl.SecureForum.model.requests.UserUpdateRequest;
+import org.etf.unibl.SecureForum.repositories.CodeVerificationRepository;
 import org.etf.unibl.SecureForum.repositories.UserRepository;
 import org.etf.unibl.SecureForum.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 
 @Service
@@ -22,18 +28,25 @@ public class UserServiceImpl extends CrudJpaService<UserEntity, Integer> impleme
 
     private final ModelMapper modelMapper;
     private final UserRepository userRepository;
+    private final CodeVerificationRepository codeVerificationRepository;
+    private final EmailSender emailSender;
 
-    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository)
+    @Autowired
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository,
+                           CodeVerificationRepository codeVerificationRepository, EmailSender emailSender)
     {
         super(userRepository, modelMapper, UserEntity.class); //For implementing CRUD operations
         this.modelMapper = modelMapper;
         this.userRepository = userRepository;
+        this.codeVerificationRepository = codeVerificationRepository;
+        this.emailSender = emailSender;
     }
 
 
     @Override
     public User signUp(SignUpRequest request) {
-        UserEntity userForDatabase = new UserEntity();
+        UserEntity userForDatabase = new UserEntity(); //Entity to place in database
+        User userToReturn = new User(); //RETURNING DTO Object
 
 
         PasswordEncoder encoder = getBCryptEncoder();   //Getting encoder for hashing
@@ -52,16 +65,46 @@ public class UserServiceImpl extends CrudJpaService<UserEntity, Integer> impleme
         }
 
         UserEntity savedUser = userRepository.save(userForDatabase);
+
+        if(savedUser.getStatus().equals(UserEntity.Status.REQUESTED))
+        {
+            String randomGeneratedCode = generateRandomVerificationCode();
+
+            CodeVerificationEntity codeVerificationEntity = new CodeVerificationEntity();
+            codeVerificationEntity.setVerificationCode(randomGeneratedCode);
+            codeVerificationEntity.setReferencedUser(savedUser);
+
+            CodeVerificationEntity savedCodeVerificationEntity =  codeVerificationRepository.save(codeVerificationEntity);
+
+            try{
+                emailSender.send(savedUser.getEmail(),
+                                buildEmail(savedUser.getUsername(), savedCodeVerificationEntity.getVerificationCode()));
+            }
+            catch(Exception ex){
+                System.out.println(ex.getLocalizedMessage());
+                throw ex;
+            }
+        }
+        ////////    SETTING DATA FOR USER TO RETURN //////////
+        userToReturn.setUsername(savedUser.getUsername());
+        userToReturn.setEmail(savedUser.getEmail());
+        userToReturn.setCreateTime(savedUser.getCreateTime());
+        userToReturn.setType(savedUser.getType());
+        userToReturn.setStatus(savedUser.getStatus());
+        /////////////////////////////////////////////////////
+        return userToReturn;
     }
 
     @Override
     public User changeStatus(Integer userId, ChangeStatusRequest request) {
         //TODO: IMPLEMENT METHOD
+        return null;
     }
 
     @Override
     public User changeRole(Integer userId, ChangeRoleRequest request) {
         //TODO: IMPLEMENT METHOD
+        return null;
     }
 
     @Override
@@ -73,4 +116,35 @@ public class UserServiceImpl extends CrudJpaService<UserEntity, Integer> impleme
     private PasswordEncoder getBCryptEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    private String generateRandomVerificationCode()
+    {
+        Random rand = new Random();
+        final int NUM_OF_CODE_DIGITS = 6; //DIGITS EXISTING IN CODE
+
+        String codeToReturn = "";
+        for(int i = 0; i < NUM_OF_CODE_DIGITS; ++i) {
+            codeToReturn += rand.nextInt(10);
+        }
+
+        return codeToReturn;
+    }
+
+    public String buildEmail(String name, String code) {
+        String textContent = "Hi " + name + ",\n\n" +
+                "Thank you for registering to MySecureForum. Please use the code below to activate your account:\n" +
+                code + "\n\n" +
+                "We are eagerly waiting for you!";
+
+        String htmlContent = "<body>" +
+                "<p>Hi " + name + ",</p>" +
+                "<p>Thank you for registering to MySecureForum. Please use the code below to activate your account:</p>" +
+                "<p><a href=\"" + code + "\">Activate Now</a></p>" +
+                "<p>We are eagerly waiting for you!</p>" +
+                "</body>";
+
+        // Construct a wrapper object to hold both text and HTML content
+        return textContent + "|||" + htmlContent;
+    }
+
 }
