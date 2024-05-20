9293d147d45e90b242935e0dd52624182531475a
DaniloVrancic <danilov98@hotmail.com>
2024-05-20 18:55:51 +0200

    SNI 1.11.3: Added selected-topic page for Forumer

---
+++ my-forum-front/my-forum-front-project/src/app/main-page/main-page/forumer-page/selected-topic/selected-topic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-topic',
+  standalone: true,
+  imports: [],
+  templateUrl: './selected-topic.component.html',
+  styleUrl: './selected-topic.component.css'
+})
+export class SelectedTopicComponent {
+
+}
