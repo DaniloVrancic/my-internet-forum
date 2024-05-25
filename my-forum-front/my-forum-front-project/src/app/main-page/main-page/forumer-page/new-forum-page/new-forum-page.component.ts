21cba6f64aa13d4787b2cca63e25523584f9fe80
DaniloVrancic <danilov98@hotmail.com>
2024-05-25 15:21:42 +0200

    SNI 1.11.12: Opening dialogue added elements and bound the input fields to corresponding string values.

--- my-forum-front/my-forum-front-project/src/app/main-page/main-page/forumer-page/new-forum-page/new-forum-page.component.ts
+++ my-forum-front/my-forum-front-project/src/app/main-page/main-page/forumer-page/new-forum-page/new-forum-page.component.ts
@@ -2,19 +2,25 @@ import { Component, Inject } from '@angular/core';
 import { ForumPost } from '../../../../../interfaces/forum-post';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-forum-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-forum-page.component.html',
   styleUrl: './new-forum-page.component.css'
 })
 export class NewForumPageComponent {
 
+  public inputTitle: string;
+  public inputContent: string;
+
   constructor(@Inject(MAT_DIALOG_DATA) public allPosts: ForumPost[],
   public dialogRef: MatDialogRef<NewForumPageComponent>,
   private router: Router){
+    this.inputTitle = "";
+    this.inputContent = "";
 
   }
 
