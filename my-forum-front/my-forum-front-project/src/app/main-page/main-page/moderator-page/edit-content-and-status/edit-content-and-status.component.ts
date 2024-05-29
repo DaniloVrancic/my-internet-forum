e83c355ce67e97a18c9d2ee9fc58866a10082926
DaniloVrancic <danilov98@hotmail.com>
2024-05-29 18:25:06 +0200

    SNI 1.12.7: Completed the look of the moderator page.

---
+++ my-forum-front/my-forum-front-project/src/app/main-page/main-page/moderator-page/edit-content-and-status/edit-content-and-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-content-and-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-content-and-status.component.html',
+  styleUrl: './edit-content-and-status.component.css'
+})
+export class EditContentAndStatusComponent {
+
+}
