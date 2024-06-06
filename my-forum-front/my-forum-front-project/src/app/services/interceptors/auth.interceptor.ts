a9c1d3d114c56e8650b677af7907af5390bb1a76
DaniloVrancic <danilov98@hotmail.com>
2024-06-06 15:44:38 +0200

    SNI 1.15.3: Interceptor edited.

--- my-forum-front/my-forum-front-project/src/app/services/interceptors/auth.interceptor.ts
+++ my-forum-front/my-forum-front-project/src/app/services/interceptors/auth.interceptor.ts
@@ -1,12 +1,18 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { UserService } from '../user.service';
+import { inject } from '@angular/core';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = JSON.stringify(sessionStorage.getItem(environment.tokenStorageKey));
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+  const userService = inject(UserService);
+  const token = userService.getJwtToken();
 
-  if (token) {
+  if (token != null && token.length > 0) {
     const cloned = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
+      setHeaders: {
+        authorization: token,
+      }
     });
     return next(cloned);
   } else {
