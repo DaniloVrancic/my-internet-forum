dbda66b60973a8d9256627c0969286fd57d9c16f
DaniloVrancic <danilov98@hotmail.com>
2024-06-06 15:34:22 +0200

    SNI 1.15.2: Added interceptor in Angular applicaton.

---
+++ my-forum-front/my-forum-front-project/src/app/services/interceptors/auth.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = JSON.stringify(sessionStorage.getItem(environment.tokenStorageKey));
+
+  if (token) {
+    const cloned = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    return next(cloned);
+  } else {
+    return next(req);
+  }
+
+};
